9da30579322a4ddcc481ee1c44d7e0fe3a280da8
Yan_Gonçalves_Santana <78172273+Yan-Santana@users.noreply.github.com>
2023-10-08 01:27:53 -0400

    Update NotificaçãoWin10.py

--- NotificaçãoWin10.py
+++ NotificaçãoWin10.py
@@ -1,9 +1,6 @@
 from win10toast import ToastNotifier
 
-#inicializa 
 toaster = ToastNotifier ()
-
-#passa parâmetros e mostra a notificação 
 toaster.show_toast(
     "Um DEV detectado!",
     "Você acessou meu repositorio, sinta-se a vontade aqui!",
@@ -11,4 +8,3 @@ toaster.show_toast(
     icon_path=None,
     duration=10
 )
-
